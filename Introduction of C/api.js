b97a8fce3b6293a6ea8186627b1759c0904644cd
Ripon Chandra Saha <riponsaha199@gmail.com>
2025-07-12 23:45:47 +0600

    Updated Code

--- Introduction of C/api.js
+++
@@ -1,6 +0,0 @@
-const searchForm = document.querySelector('form');
-const movieContainer = document.querySelector('.movie-container');
-const inputBox = document.querySelector('.inputBox');
-searchForm.addEventListener('submit', () => {
-    console.log(inputBox.value);
-});
--- Introduction of C/com.js
+++
@@ -1,5 +0,0 @@
-const loadComment = () => {
-    fetch('https://jsonplaceholder.typicode.com/comments')
-        .then(res => res.json())
-        .then(data => console.log(data));
-}
