30124f4453063790d1bf5a941ecb05dcbe028e1b
Ripon Chandra Saha <riponsaha199@gmail.com>
2025-07-13 11:04:20 +0600

    Updated Code

--- STL list and Cycle Detection/stl_list.cpp
+++ STL list and Cycle Detection/stl_list.cpp
@@ -3,9 +3,15 @@ using namespace std;
 int main()
 {
     list<int> b = {1, 5, 8, 3, 75};
-    int a[] = {10, 565, 56};
-    list<int> b2(a, a + 3);
-    for (int val :b2)
+    // int a[] = {10, 565, 56};
+    // list<int> b2(a, a + 3);
+    b.push_back(40);
+    b.push_front(100);
+
+    b.pop_back();
+    b.pop_back();
+    b.pop_front();
+    for (int val : b)
     {
         cout << val << endl;
     }
