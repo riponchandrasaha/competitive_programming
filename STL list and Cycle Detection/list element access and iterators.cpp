b62353727feba7b198a50b7e1398d54478e8fa8f
Ripon Chandra Saha <riponsaha199@gmail.com>
2025-07-13 19:53:07 +0600

    updated code

---
+++ STL list and Cycle Detection/list element access and iterators.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> l = {41, 65, 89, 32, 78, 14, 9};
+    /* cout << l.back() << endl;
+    cout << l.front() << endl;
+    cout << *next(l.begin(), 2) << endl; */
+
+    
+    return 0;
+}
---
+++ STL list and Cycle Detection/list operation function.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> l = {4, 8, 65, 6, 5, 5, 9, 8, 23, 9, 4, 54, 17};
+    //l.remove(10);
+    //l.sort();
+    //l.sort(greater<int>());
+    //l.reverse();
+    for (int val : l)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
